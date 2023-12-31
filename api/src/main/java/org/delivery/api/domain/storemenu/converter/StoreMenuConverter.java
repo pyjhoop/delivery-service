26678daab149b7ddca86c03acb30b0787ec336ee
pyjhoop <pyjhoop@naver.com>
2023-12-31 13:06:06 +0900

    #12 Store-menu 데이터베이스 개발 및 서비스 로직 개발
    - 워크벤치에서 Store_menu 테이블 생성
    - Entity, Repository 생성 ** 엔티티에 고정값이 들어가야 하면 enum 사용하고 엔티티에 꼭 @Enumerate()사용
    - buisness, converter, service, controller 생성

---
+++ api/src/main/java/org/delivery/api/domain/storemenu/converter/StoreMenuConverter.java
@@ -0,0 +1,50 @@
+package org.delivery.api.domain.storemenu.converter;
+
+import org.delivery.api.common.annotation.Converter;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.exception.ApiException;
+import org.delivery.api.domain.storemenu.controller.model.StoreMenuRegisterRequest;
+import org.delivery.api.domain.storemenu.controller.model.StoreMenuResponse;
+import org.delivery.db.storemenu.StoreMenuEntity;
+import org.delivery.db.storemenu.StoreMenuRepository;
+
+import java.util.Optional;
+
+@Converter
+public class StoreMenuConverter {
+
+    public StoreMenuEntity toEntity(StoreMenuRegisterRequest request){
+
+        return Optional.ofNullable(request)
+                .map(it->{
+                    return StoreMenuEntity.builder()
+                            .storeId(request.getStoreId())
+                            .name(request.getName())
+                            .amount(request.getAmount())
+                            .thumbnailUrl(request.getThumbnailUrl())
+                            .build();
+                })
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
+
+    }
+
+    public StoreMenuResponse toResponse(StoreMenuEntity storeMenuEntity){
+
+        return Optional.ofNullable(storeMenuEntity)
+                .map(it->{
+
+                    return StoreMenuResponse.builder()
+                            .id(it.getId())
+                            .name(it.getName())
+                            .storeId(it.getStoreId())
+                            .amount(it.getAmount())
+                            .status(it.getStatus())
+                            .thumbnailUrl(it.getThumbnailUrl())
+                            .likeCount(it.getLikeCount())
+                            .sequence(it.getSequence())
+                            .build();
+
+                })
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
+    }
+}
