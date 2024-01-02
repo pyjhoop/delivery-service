26ee98429ed37a1bab94efd12b26ae3ee49988c6
pyjhoop <pyjhoop@naver.com>
2024-01-02 12:05:59 +0900

    #17 가맹점 서버 consumer 설정

---
+++ store-admin/src/main/java/org/delivery/storeadmin/config/objectmapper/ObjectMapperConfig.java
@@ -0,0 +1,33 @@
+package org.delivery.storeadmin.config.objectmapper;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        var objectMapper = new ObjectMapper();
+
+        objectMapper.registerModule(new Jdk8Module()); //jdk 8버전 이후 클래스를 시리얼라이징 또는 디시리얼라이징 하는 모듈
+        objectMapper.registerModule(new JavaTimeModule()); // LocalDateTime
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); // 모르는 json field에 대해서 무시한다.
+        // 즉 이메일만 필요한데 이메일 외의 다른 속성도 넣어서 보내면 에러를 발생시킬 것인가에 대한 속성
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false); // 비어있는것 생성할때
+
+        // 날짜 관련 직렬화
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        // 스네이크 케이스
+        objectMapper.setPropertyNamingStrategy(new PropertyNamingStrategies.SnakeCaseStrategy());
+
+        return objectMapper;
+    }
+}
